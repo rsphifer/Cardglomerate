063cb7723a684232858845908edb2bb019f2f896
ncool <ncool@purdue.edu>
2014-10-15 19:23:34 -0400

    Added card pictures and War GUI

--- GUI/Cardglomerate/src/view/WarGame.java
+++ GUI/Cardglomerate/src/view/WarGame.java
@@ -8,6 +8,13 @@ public class WarGame extends BasicGameState {
 	
 	private String mouse = "No input yet";
 	private Image background;
+	private Image curCard1;
+	private Image curCard2;
+	private Image deckImage;
+	private Image[] clubs;
+	private Image[] hearts;
+	private Image[] spades;
+	private Image[] diamonds;
 
 	
 	public WarGame(int state) {
@@ -15,8 +22,32 @@ public class WarGame extends BasicGameState {
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
 		//initialize and scale background image
-		background = new Image("res/beach.jpg");
+		background = new Image("res/Green Background.jpg");
 		background = background.getScaledCopy(1280, 720);
+		
+		//initialize flat deck image
+		deckImage = new Image("res/Cards/b2fv.png");
+		
+		//initalize deck arrays
+		clubs = new Image[13];
+		hearts = new Image[13];
+		spades = new Image[13];
+		diamonds = new Image[13];
+		for (int i = 1; i < 14; i++) {
+			clubs[i-1] = new Image("res/Cards/c" + i + ".png");
+		}
+		
+		for (int i = 1; i < 14; i++) {
+			hearts[i-1] = new Image("res/Cards/h" + i + ".png");
+		}
+		
+		for (int i = 1; i < 14; i++) {
+			spades[i-1] = new Image("res/Cards/s" + i + ".png");
+		}
+		
+		for (int i = 1; i < 14; i++) {
+			diamonds[i-1] = new Image("res/Cards/d" + i + ".png");
+		}
 		
 
 	}
@@ -28,6 +59,14 @@ public class WarGame extends BasicGameState {
 		//mouse coordinates for testing / building
 		g.drawString(mouse, 10, 25);
 		
+		//draw deck image twice
+		deckImage.draw(550, 25);
+		deckImage.draw(650, 550);
+		
+		//draw current cards
+		curCard1.draw(600, 200);
+		curCard2.draw(600, 325);
+		
 		
 	}
 	
@@ -35,7 +74,11 @@ public class WarGame extends BasicGameState {
 		//mouse coordinates
 		int xpos = Mouse.getX();
 		int ypos = Mouse.getY();
-		mouse = "Mouse position x: " + xpos + " y: " + ypos;		
+		mouse = "Mouse position x: " + xpos + " y: " + ypos;	
+		
+		//temp curcards
+		curCard1 = diamonds[5];
+		curCard2 = hearts[11];
 		
 		
 	}
