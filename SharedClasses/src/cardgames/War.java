255d70720551b5d061ffbfe6bc8bb1615ffbef22
Jordan Mounts <jmounts234@gmail.com>
2014-10-16 17:17:45 -0400

    updated player, cardgame, war, and card classes

--- SharedClasses/src/cardgames/War.java
+++ SharedClasses/src/cardgames/War.java
@@ -2,7 +2,7 @@ package cardgames;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Stack;
+import java.util.LinkedList;
 
 import player.Player;
 import cards.Card;
@@ -15,19 +15,35 @@ public class War extends CardGame implements Serializable{
 	}
 
 	private ArrayList<Player> players;
-	private Stack<Card> winnings;
+	private LinkedList<Card> winnings;
+	private CardGame cardGame;
+	private boolean gameOver = false;
 	
+<<<<<<< HEAD
+	public War(ArrayList<Player> playerNames){
+		this.players = playerNames;
+		winnings = new LinkedList<Card>();
+		this.cardGame = new CardGame();
+		cardGame.connect();
+		play();
+	}
+=======
 
+>>>>>>> 1df86365017ae4f6895df380ca0591b77fcbe08d
 
 	private void roundWin(Player winner){
-		while(!this.winnings.empty()){
+		while(!this.winnings.isEmpty()){
 			winner.addCardToHand((Card)this.winnings.remove(0));
 		}
 	}
 
 	private void roundTie(){
 		//model.war();
+<<<<<<< HEAD
+		System.out.println("WAR!");
+=======
 		
+>>>>>>> 1df86365017ae4f6895df380ca0591b77fcbe08d
 		Card p1Down = this.players.get(0).playTopCard();
 		this.winnings.push(p1Down);
 		Card p1Up = this.players.get(0).playTopCard();
@@ -44,7 +60,19 @@ public class War extends CardGame implements Serializable{
 	}
 
 	private void score(Card c1, Card c2){
+<<<<<<< HEAD
+		//model.displayThese2Cards(c1,c2,h1size,h2size);
+		if(c1==null) {
+			this.gameOver = true;
+			return;
+		}
+		if(c2==null) {
+			this.gameOver = true;
+			return;
+		}
+=======
 		//model.displayThese2Cards(c1,c2);
+>>>>>>> 1df86365017ae4f6895df380ca0591b77fcbe08d
 		
 		if(c1.getPower() > c2.getPower()){
 			roundWin(this.players.get(0));
@@ -58,19 +86,52 @@ public class War extends CardGame implements Serializable{
 	private void win(Player winner){
 		//model.win(winner.userName);
 		System.out.println(winner.userName+" won!");
+		
 	}
 
 	public void play(){
-
-		while(this.players.get(0).getHandSize() != 0 || this.players.get(1).getHandSize() != 0){
+		/*for(int i=1;i<=13;i++){
+			this.players.get(0).addCardToHand(new Card(i,0));
+		}
+		for(int i=1;i<=13;i++){
+			this.players.get(0).addCardToHand(new Card(i,1));
+		}
+		for(int i=1;i<=13;i++){
+			this.players.get(0).addCardToHand(new Card(i,2));
+		}
+		for(int i=2;i<=13;i++){
+			this.players.get(0).addCardToHand(new Card(i,3));
+		}
+		this.players.get(1).addCardToHand(new Card(1,3));
+		*/
+		
+		for(int i=0;i<52;i++){
+			if(i%2 == 0){
+				this.players.get(1).addCardToHand(this.cardGame.getTopOfDeck());
+			} else {
+				this.players.get(0).addCardToHand(this.cardGame.getTopOfDeck());
+			}
+		}
+		int j=0;
+		while(!gameOver){
+			System.out.println("Turn: "+(j++));
 			Card p1Card = this.players.get(0).playTopCard();
 			this.winnings.push(p1Card);
 			Card p2Card = this.players.get(0).playTopCard();
 			this.winnings.push(p2Card);
+<<<<<<< HEAD
+			/*while(model.isWaiting()){
+				Thread.sleep(1000);
+			}*/
+=======
 			//while(model.isWaiting()){
 			//	Thread.sleep(1000);
 			//}
+>>>>>>> 1df86365017ae4f6895df380ca0591b77fcbe08d
 			this.score(p1Card, p2Card);
+			if(this.players.get(0).getHandSize() == 0 || this.players.get(1).getHandSize() == 0){
+				this.gameOver = true;
+			}
 		}
 		if(this.players.get(0).getHandSize() == 0){
 			this.win(this.players.get(1));
--- SharedClasses/src/player/Player.java
+++ SharedClasses/src/player/Player.java
@@ -1,6 +1,7 @@
 package player;
 
 import java.io.Serializable;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Queue;
 
@@ -11,13 +12,15 @@ import cards.Card;
 public class Player implements Serializable{
 
 	private ArrayList<Player> friends;
-	private Queue<Card> hand;
-	private Queue<Card> discard;
+	private ArrayDeque<Card> hand;
+	private ArrayList<Card> discard;
 	private int money;
 	public String userName;
 	private String password;
 	
 	public Player(String playerName, String password){
+		this.hand = new ArrayDeque<Card>();
+		this.discard = new ArrayList<Card>();
 		this.password = password;
 		this.userName = playerName;
 	}
@@ -26,11 +29,20 @@ public class Player implements Serializable{
 		return this.hand.size();
 	}
 	
+	public void getMoney(int money){
+		this.money += money;
+	}
+	
 	public void addCardToHand(Card newCard){
+		System.out.println(this.userName+" got a "+newCard.getPower() + " of " + newCard.getSuit());
 		this.hand.add(newCard);
 	}
 
 	public Card playTopCard(){
-		return this.hand.remove();
+		if(!this.hand.isEmpty()){
+			return this.hand.remove();
+		} else {
+			return null;
+		}
 	}
 }
